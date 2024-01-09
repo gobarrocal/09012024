747d4c25a3ea9949269e797c251fd4dfd9560cae
Gabriel Barrocal <156017411+gobarrocal@users.noreply.github.com>
2024-01-09 15:09:31 -0300

    Add files via upload

---
+++ IntegerType.java
@@ -0,0 +1,9 @@
+public class IntegerType {
+    
+    public static void main(String[] args) {
+        
+        int intVar = 9;  
+        
+        System.out.println(intVar);
+    }
+}
---
+++ MathAbsolute.java
@@ -0,0 +1,9 @@
+public class MathAbsolute {
+    
+public static void main(String[] args) {
+    
+    double matVar = Math.abs(-200.000);
+    
+    System.out.println(matVar );
+}
+}
