ea1040383ce0b9b4213365ad378058239a6042bd
Gabriel Barrocal <156017411+gobarrocal@users.noreply.github.com>
2024-01-09 15:55:53 -0300

    Add files via upload

---
+++ MathMax.java
@@ -0,0 +1,9 @@
+public class MathMax {
+   public static void main(String[] args) {
+    
+    double a = 1.200;
+    double b = 1.500;
+
+    System.out.println(Math.max (a, b));
+   } 
+}
---
+++ MathMin.java
@@ -0,0 +1,10 @@
+public class MathMin {
+    public static void main(String[] args) {
+        
+        double a = 15.0;
+        double b = 25.0;
+        System.out.println(Math.min(a,b));
+
+    }
+
+}
